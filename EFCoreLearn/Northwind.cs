12cc9fd055d745de880256d126cacd11a851a49a
Jan Swierblewski <jan.swierblewski06@gmail.com>
2026-09-10 16:22:33 +0200

    fix: update namespaces for Category and Product models to use Northwind.Models

--- EFCoreLearn/Northwind.cs
+++ EFCoreLearn/Northwind.cs
@@ -1,5 +1,5 @@
 using Microsoft.EntityFrameworkCore;
-using Models;
+using Northwind.Models;
 
 namespace EFCoreLearn.Data;
 
--- Northwind.Models/Category.cs
+++ Northwind.Models/Category.cs
@@ -1,6 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
 
-namespace Models;
+namespace Northwind.Models;
 
 public class Category
 {
