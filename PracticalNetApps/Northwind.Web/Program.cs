c9822ba5eb6c0789fec8b85133077d4cd27f8fe5
Jan Swierblewski <jan.swierblewski06@gmail.com>
2026-09-16 19:45:41 +0200

    feat: enhance server configuration with endpoint logging

--- PracticalNetApps/Northwind.Web/Program.cs
+++ PracticalNetApps/Northwind.Web/Program.cs
@@ -1,5 +1,3 @@
-using EntityModels;
-
 #region www server configure
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
@@ -13,11 +11,31 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseHsts();
 }
+
+app.Use(async (HttpContext context, Func<Task> next) =>
+{
+
+    RouteEndpoint? rep = context.GetEndpoint() as RouteEndpoint;
+
+    if(rep is not null)
+    {
+        WriteLine($"Endpoint name: {rep.DisplayName}");
+        WriteLine($"Endpoint's path pattern: {rep.RoutePattern.RawText}");
+    }
+    if(context.Request.Path == "/bonjour")
+    {
+        await context.Response.WriteAsync("Bonjour!");
+        return;
+    }
+    await next();
+});
+
 app.UseHttpsRedirection();
-#endregion
 
 app.UseDefaultFiles();
 app.UseStaticFiles();
+
+#endregion
 
 app.MapRazorPages();
 app.MapGet("/hi", () => $"Actual environment is {app.Environment.EnvironmentName}");
