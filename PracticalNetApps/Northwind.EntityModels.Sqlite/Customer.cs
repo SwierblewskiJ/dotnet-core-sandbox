59df6b09a13af833564c3a68faf3ffb1891bb0dd
Jan Swierblewski <jan.swierblewski06@gmail.com>
2026-09-18 15:16:47 +0200

    feat: update Customer model with XML serialization and inverse property, enhance logging configuration

--- PracticalNetApps/Northwind.EntityModels.Sqlite/Customer.cs
+++ PracticalNetApps/Northwind.EntityModels.Sqlite/Customer.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.EntityFrameworkCore;
+using System.Xml.Serialization;
 
 namespace EntityModels;
 
@@ -46,6 +47,7 @@ public partial class Customer
     [Column(TypeName = "nvarchar (24)")]
     public string? Fax { get; set; }
 
-    [InverseProperty("Customer")]
+    [InverseProperty(nameof(Order.Customer))]
+    [XmlIgnore]
     public virtual ICollection<Order> Orders { get; set; } = new List<Order>();
 }
